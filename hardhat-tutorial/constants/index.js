72933202d80c301027d7f31bb666f487e65127f3
Taylor Ferran <tailzpk@hotmail.co.uk>
2022-08-06 12:38:42 +0100

    Update constants/contract to use my vercel deployment

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xe71426B32524ce4D04B5FFd91128D84d6fedFDED";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-coral-delta.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
